4734058def993d476c1ebf95107a27bd0e45683c
chirag8850 <c.vaviya@somaiya.edu>
2026-03-19 10:43:27 +0530

    setup prettier eslint and husky

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default defineConfig([
+    globalIgnores(["dist/**", "node_modules/**"]),
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            "no-console": "warn",
+        },
+    },
+    prettierConfig,
+]);
