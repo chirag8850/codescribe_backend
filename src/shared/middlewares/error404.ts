11e6ba9e0f211517598cdd18d312f8c21189d8d2
chirag8850 <c.vaviya@somaiya.edu>
2026-03-22 12:52:33 +0530

    created 404 error handler

--- src/shared/middlewares/error404.ts
+++ src/shared/middlewares/error404.ts
@@ -1,11 +1,12 @@
 import type { NextFunction, Request, Response } from 'express';
-import { sendError } from '../utils/apiResponse.js';
 import { HTTP_STATUS } from '../constants/httpStatus.js';
+import { ApiError } from '../utils/apiError.js';
 
-export const notFoundHandler = (_req: Request, res: Response, _next: NextFunction) => {
-    return sendError({
-        res,
-        message: `Route ${_req.method} ${_req.originalUrl} not found`,
-        statusCode: HTTP_STATUS.NOT_FOUND,
-    });
+export const notFoundHandler = (req: Request, _res: Response, next: NextFunction) => {
+    next(
+        new ApiError({
+            message: `Route ${req.method} ${req.originalUrl} not found`,
+            statusCode: HTTP_STATUS.NOT_FOUND,
+        }),
+    );
 };
